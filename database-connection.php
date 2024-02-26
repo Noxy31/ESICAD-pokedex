0be4edf682d6c0cfd75bca0f3c636115c3da4dd0
Noxy31 <nellenoar@gmail.com>
2024-02-26 15:14:35 +0100

    Added list and done database connexion

--- database-connection.php
+++ database-connection.php
@@ -2,6 +2,10 @@
 global $databaseConnection;
 
 /* A COMPLETER : remplacer les paramètres pour se connecter à votre base de données de pokémon */
-$databaseConnection = mysqli_connect("localhost", "root", "", "pokemontp2") or die("Erreur de connexion: " . mysqli_error($connexion));
+$databaseConnection = mysqli_connect("localhost", "root", "", "pokemontp2") or die("Erreur de connexion: " . mysqli_error($databaseConnection));
 
-$result = mysqli_query($connexion, "Select * contact (name, email, message) VALUES ('$nom', '$email', '$message')");
+if (!$databaseConnection) {
+    // en cas d'erreur, la fonction mysqli_connect_error() indique la cause de l'échec de la connexion
+    throw new RuntimeException("Cannot connect to the database. Cause : " . mysqli_connect_error());
+   }
+?>
--- list.php
+++ list.php
@@ -1,17 +1,25 @@
 <!-- 
     Ce fichier représente la page de liste de tous les pokémons.
 -->
+<
 <?php
 require_once("head.php");
+require_once("database-connection.php");
+$databaseConnection;
+$query = $databaseConnection->query("SELECT * from `pokemon`");
+
+if (!$query) {
+    throw new RuntimeException("Cannot execute query. Cause : " . mysqli_error($databaseConnection));
+} else {
+    $result = $query->fetch_all(MYSQLI_ASSOC);
+    foreach ($result as $row) {
+        echo "<tr><td>" . $row["IdPokemon"] . "</td><td>" . $row['NomPokemon'] . "</td><td><img src='" . $row["urlPhoto"] . "'></td><td>" . $row["IdTypePokemon"] . "</td></tr>";
+    }
+}
+
 ?>
 <pre>
-    &lt;
-    A REMPLACER PAR VOTRE CODE POUR CHARGER ET AFFICHER DANS UN TABLEAU LA LISTE DES POKEMONS PAR LEUR NOM.
-    CHAQUE POKEMON DOIT ETRE CLIQUABLE POUR NAVIGUER SUR UNE PAGE OU L'ON AFFICHE SON IMAGE ET L'ENSEMBLE DE SES CARACTERISTIQUES 
-    &gt;
     </pre>
-
-
 <?php
 require_once("footer.php");
 ?>
