a85fbaaa4788feb3ace1a890981bd7687899caec
Noxy31 <nellenoar@gmail.com>
2024-02-26 12:20:51 +0100

    modified:   database-connection.php

--- database-connection.php
+++ database-connection.php
@@ -2,4 +2,6 @@
 global $databaseConnection;
 
 /* A COMPLETER : remplacer les paramètres pour se connecter à votre base de données de pokémon */
-$databaseConnection = mysqli_connect("localhost", "root", "", "portfolio_form") or die("Erreur de connexion: " . mysqli_error($connexion));
+$databaseConnection = mysqli_connect("localhost", "root", "", "pokemontp2") or die("Erreur de connexion: " . mysqli_error($connexion));
+
+$result = mysqli_query($connexion, "Select * contact (name, email, message) VALUES ('$nom', '$email', '$message')");
