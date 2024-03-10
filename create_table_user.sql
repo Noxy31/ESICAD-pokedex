2244063017f03dbad8071ae983537e42b586ff8a
Noxy31 <nellenoar@gmail.com>
2024-03-10 19:46:01 +0100

    modified sql script

--- create_table_user.sql
+++ create_table_user.sql
@@ -1,15 +1,16 @@
 /*
- TypePokemon(IdTypePokemon, libelleType)
- 
- Pokemon(Id_Pokemon, nomPokemon, urlPhoto, PV, Attaque, Defense,
- Vitesse, Special, #IdTypePokemon, #IdSecondTypePokemon)
- 
- EvolutionPokemon(#idPokemon, #idEvolution)
- 
- User(idUser, firstName, lastName, login, passwordHash)
- 
- User_Pokemon(#idUser, #idPokemon, captureDate) 
- */
+TypePokemon(IdTypePokemon, libelleType)
+
+Pokemon(Id_Pokemon, nomPokemon, urlPhoto, PV, Attaque, Defense,
+        Vitesse, Special, #IdTypePokemon, #IdSecondTypePokemon)
+
+EvolutionPokemon(#idPokemon, #idEvolution)
+
+User(idUser, firstName, lastName, login, passwordHash)
+
+User_Pokemon(#idUser, #idPokemon, captureDate) 
+*/
+
 CREATE TABLE users (
     idUser INT PRIMARY KEY AUTO_INCREMENT,
     firstName VARCHAR(50),
@@ -17,6 +18,7 @@ CREATE TABLE users (
     login VARCHAR(50) UNIQUE,
     passwordHash VARCHAR(255)
 );
+
 CREATE TABLE user_pokemon (
     idUser INT,
     idPokemon INT,
@@ -25,6 +27,6 @@ CREATE TABLE user_pokemon (
     FOREIGN KEY (idUser) REFERENCES users(idUser),
     FOREIGN KEY (idPokemon) REFERENCES Pokemon(idPokemon)
 );
+
 ALTER TABLE user_pokemon DROP PRIMARY KEY;
-ALTER TABLE user_pokemon
-ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY FIRST;
+ALTER TABLE user_pokemon ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY FIRST;
