b56aa866763872505eeec6a0905294f3e525b0c2
Noxy31 <nellenoar@gmail.com>
2024-03-10 19:35:08 +0100

    added comments to menu.php

--- menu.php
+++ menu.php
@@ -12,15 +12,14 @@
         </a>
         <?php
         if (session_status() == PHP_SESSION_NONE) {
-            session_start();
+            session_start(); // session start si c'est pas deja le cas
         }
 
         if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['logout'])) {
-            // Déconnexion de l'utilisateur
             session_unset();
             session_destroy();
             header("Location: index.php");
-            exit;
+            exit; // prend en charge la deconexion avec session_destroy
         }
         ?>
         <a href="login.php">
@@ -31,14 +30,10 @@
         <?php
         if (isset($_SESSION['idUser'])) {
             echo '<a href="pokedex-utilisateur.php"><li>Pokedex Utilisateur</li></a>';
-
-            // Affiche le bandeau de bienvenue en bas du menu
-            echo "<div class='welcome-banner'>Bienvenue, " . $_SESSION['firstName'] . " " . $_SESSION['lastName'] . " !</div>";
-
-            // Ajoute le bouton de déconnexion
+            echo "<div class='welcome-banner'>Bienvenue, " . $_SESSION['firstName'] . " " . $_SESSION['lastName'] . " !</div>"; // bandeau de bienvenue
             echo '<form method="post" id="logoutForm">
                       <button type="submit" name="logout" id="logoutButton">Déconnexion</button>
-                  </form>';
+                  </form>'; // bouton de deconexxion
         }
         ?>
     </ul>
